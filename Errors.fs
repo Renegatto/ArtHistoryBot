7cd8b2958edae9d08893f2e97a0ea23414b79e3c
Renegatto <edittorr@mail.ru>
2020-04-06 21:07:14 +0300

    Errors.fs moved in hierarchy

---
+++ Errors.fs
@@ -0,0 +1,12 @@
+﻿module Errors
+type DatabaseError = DatabaseError of string
+type StorageError = StorageError of string
+type DomainError =
+    |TestGeneratorNotFounded of sid:int
+type SubscriptionError =
+    |NoSubscriptionFound of int
+type Error = 
+    |Database of DatabaseError
+    |Storage of StorageError
+    |Domain of DomainError
+    |Subscription of SubscriptionError

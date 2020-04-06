9b999b98aca7c4b80456bf6232affb8a2246cd75
Renegatto <edittorr@mail.ru>
2020-04-06 21:49:27 +0300

    distinction: commands.fs and commandhandler.fs

---
+++ Commands/CommandHandler.fs
@@ -0,0 +1,44 @@
+﻿module CommandHandler
+open Errors
+open Domain
+open Infrastructure
+open MainIO
+open Events
+
+type GuessResultCommand = {
+    sub_id : Subscriptions.SubscriptionId
+}
+type NewtTestCommand = {
+    sub_id : Subscriptions.SubscriptionId
+}
+type NextTestCommand = {
+    sub_id : Subscriptions.SubscriptionId
+}
+type Command = 
+    |GuessResult of GuessResultCommand
+    |NewTest of NewtTestCommand
+    |NextTest of NextTestCommand
+
+type EventPublisher = EventPublisher of (DomainEvent -> unit Async)
+type Commands = Commands of Command []
+type CommandProcessor = (Command -> Result<DomainEvent,Error>)
+type CommandMatcher = CommandMatcher of (Command -> Subscriptions.SubscriptionId*CommandProcessor)
+
+type CommandHandler = CommandHandler with //CommandHandler of (Command -> Result<DomainEvent,Error>)
+    static member private handleCommand (command:Command) 
+        (EventPublisher publish) 
+        (CommandMatcher matcher)
+        : Asyncresult<DomainEvent,Error> = asyncresult {
+
+        let (sid,processor) = matcher command
+        let! stored_data = Subscriptions.readData sid
+        let event: Result<DomainEvent,Error> = processor command // stored_data
+
+        Result.map publish event |> ignore
+
+        return! event |> Asyncresult.fromResult
+    }
+    static member handle = CommandHandler.handleCommand
+    
+
+let mutable commands = Commands Array.empty
---
+++ Commands/Commands.fs
@@ -0,0 +1,25 @@
+﻿module Commands
+open CommandHandler
+open Infrastructure
+open MainIO
+
+    let nextTest (NextTest sid): Asyncresult<Test,Error> = asyncresult {
+        match! Subscriptions.SubscriptionId sid |> Subscriptions.readData with
+        |Subscriptions.TestData (TestGenerator test_generator) ->
+            return! test_generator () |> Asyncresult.okAsync
+        |Subscriptions.NoData ->
+            return! TestGeneratorNotFounded sid |> Domain |> Asyncresult.error
+    }
+    let newTest (NextTest sid): Asyncresult<Test,Error> = asyncresult {// not impl
+        match! Subscriptions.SubscriptionId sid |> Subscriptions.readData with
+        |Subscriptions.TestData (TestGenerator test_generator) ->
+            return! test_generator () |> Asyncresult.okAsync
+        |Subscriptions.NoData ->
+            return! TestGeneratorNotFounded sid |> Domain |> Asyncresult.error
+    }
+
+[<System.Diagnostics.DebuggerDisplay("Commands: command matching")>]
+let matchCommand = function
+    |GuessResult cmd -> cmd.sub_id,0
+    |NewTest cmd -> cmd.sub_id,0
+    |NextTest cmd -> cmd.sub_id,0
